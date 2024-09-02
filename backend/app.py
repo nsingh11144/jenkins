10800d982c34d39ee75848faf147a9118d8c33f5
Robin <robinmalli11144@gmail.com>
2024-09-02 14:46:03 +0530

    polling

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ CORS(app)  # Enable CORS for all routes
 
 @app.route('/')
 def hello_world():
-    return jsonify(message='Hello from Flask!')
+    return jsonify(message='Hello from Flask and Jenkins!')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
